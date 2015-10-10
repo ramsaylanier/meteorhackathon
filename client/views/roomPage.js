e145b3529329706a3099596e6493fdddb83cf997
Peter Brandt <peter.m.brandt@gmail.com>
2015-10-10 12:24:20 -0400

    adding roomPage

---
+++ client/views/roomPage.js
@@ -0,0 +1,11 @@
+Template.roomPage.onCreated(function(){
+	var instance = this;
+
+	instance.ready = new ReactiveVar(false);
+	instance.rooms = new ReactiveVar();
+
+  
+
+	instance.autorun( function(){
+	});
+})
