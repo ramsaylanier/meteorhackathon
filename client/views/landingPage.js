1d0f6e27a66ca27c0704638fea909da6147514df
Ramsay Lanier <ramsay@nclud.com>
2015-10-10 12:57:03 -0400

    added room list to landing page

--- client/views/landingPage.js
+++ client/views/landingPage.js
@@ -1,3 +1,32 @@
+Template.landingPage.onCreated(function(){
+
+  let instance = this;
+  let cookie = Cookie.get('user');
+
+  instance.ready = new ReactiveVar(false);
+  instance.user = new ReactiveVar();
+
+  let subscription = instance.subscribe('currentUser', cookie);
+
+  instance.autorun( function(){
+    subscription = instance.subscribe('currentUser', cookie);
+
+    if (subscription.ready()){
+      instance.ready.set(true);
+    } else {
+      instance.ready.set(false);
+    }
+  });
+
+})
+
+
+Template.landingPage.helpers({
+  userRooms: function(){
+    return Users.findOne(Cookie.get('user')).rooms;
+  }
+})
+
 Template.landingPage.events({
   'submit .create-room-form': function(e){
     e.preventDefault();
@@ -5,7 +34,7 @@ Template.landingPage.events({
     let roomAttributes = {
       name: $('.room-name-field').val(),
       username: $('.room-username-field').val(),
-      cookie: Cookie.get('user')
+      creatorId: Cookie.get('user')
     }
 
     Meteor.call('createRoom', roomAttributes, function(err, res){
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('roomlist', function(){
   return Rooms.find();
 })
+
+Meteor.publish('currentUser', function(userId){
+  return Users.find({_id: userId});
+})
